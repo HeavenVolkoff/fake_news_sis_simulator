b5736fe38081dd98d3527eb4e759849dba923c5d
HeavenVolkoff <vasconcellos.dev@gmail.com>
2021-03-01 22:37:55 -0300

    Add python 3.7 support

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -17,6 +17,18 @@ class TestImport(unittest.TestCase):
             external_truth_transmission_rate=0,
         ).debug(300)
 
+    def test_example_1_rnd(self) -> None:
+        Simulator(
+            ([EventType.Fake], *([EventType.Truth] for _ in range(99))),
+            rnd=True,
+            fake_rate_heuristic=lambda _: 1,
+            truth_rate_heuristic=lambda _: 1,
+            internal_fake_transmission_rate=0.5,
+            external_fake_transmission_rate=0,
+            internal_truth_transmission_rate=0.15,
+            external_truth_transmission_rate=0,
+        ).debug(300)
+
 
 if __name__ == "__main__":
     unittest.main()
